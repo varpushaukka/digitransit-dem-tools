00f7dd77046d48c7aab81d26cb2c4689c020e598
juusokor <juseek@gmail.com>
2018-09-11 17:18:07 +0300

    refactoring and tweaked GDAL_CACHEMAX

--- download_and_optimize_dem_data.sh
+++ download_and_optimize_dem_data.sh
@@ -6,12 +6,12 @@
 
 # Usage:
 #
-#    bash download_and_optimize_dem_data.sh <area-code> <optional_clipper_file>
+#    bash download_and_optimize_dem_data.sh <area-code> [optional_clipper_file]
 #
 # Test script by running it without command line arguments. Test downloads 4 tiles map tiles from NLS API and clips it with test-clipper.json.
 # Test run takes about ~2 min. Test requires you to update the NLS ATOM Feeed API token in config.example.json
 #
-# Area-code values can be found config.example.json, "HSL", "WALTTI", "TEST".
+# Area-code values can be found config.example.json, for example "HSL", "WALTTI", "TEST".
 
 set -e
 SECONDS=0
@@ -33,22 +33,26 @@ else
 	exit 1
 fi
 
-
+output_dir="output-$dataset-$(date +"%Y%m%d_%H%M")"
 dem_data_dl_dir="km2-data-$(date +"%Y%m%d_%H%M")"
 file_paths="km2_tif_file_paths-$(date +"%Y%m%d_%H%M").txt"
+dataset_vrt="$dataset-$(date +"%Y%m%d_%H%M").vrt"
 
-echo "Starting download script for raster data from NLS API"
-mkdir -p $dem_data_dl_dir
-python km2-dem-downloader.py config.json $dataset $dem_data_dl_dir -v
+echo "1. Starting download script km2-dem-downloader.py for getting raster elevation data from NLS API"
+mkdir -p $output_dir/$dem_data_dl_dir
+python km2-dem-downloader.py config.json $dataset $output_dir/$dem_data_dl_dir -v
 
 echo "Running gdal_create_optimized_dem.sh DEM clipper and optimizer for the dataset"
 
-find $dem_data_dl_dir -name '*.tif' > $file_paths
+find $output_dir/$dem_data_dl_dir -name '*.tif' > $output_dir/$file_paths
+
+echo "Creating VRT $dataset_vrt of raster files in $file_paths"
+gdalbuildvrt -input_file_list $output_dir/$file_paths $output_dir/$dataset_vrt
 
 if $noclip ; then
-    bash gdal_create_optimized_dem.sh $file_paths 
+    bash gdal_create_optimized_dem.sh $output_dir/$dataset_vrt $output_dir
 else
-    bash gdal_create_optimized_dem.sh $file_paths $clipper
+    bash gdal_create_optimized_dem.sh $output_dir/$dataset_vrt $output_dir $clipper
 fi
 
 duration=$SECONDS
--- gdal_create_optimized_dem.sh
+++ gdal_create_optimized_dem.sh
@@ -8,7 +8,7 @@
 #	add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update && apt-get install gdal-bin
 #
 # Input:
-#   - List of source tif file paths for the DEM. (Eg. find $(pwd) -iname "*.tif" -type f > raster_paths.txt)
+#   - GDAL supported raster formats, eg. .VRT, GeoTIFF
 #   - Optional OGR supported geometry format for clipping the dataset to a geometry cutline. For example GeoJSON.
 #
 # Output:
@@ -16,7 +16,7 @@
 #     integers rounded to decimeters from meters.
 #
 # Usage:
-#   gdal_create_optimized_dem.sh <input_file_list> [optional_clipper]
+#   gdal_create_optimized_dem.sh <input_file> <output_dir> [optional_clipper]
 #
 # Author: Juuso Korhonen
 
@@ -29,48 +29,52 @@ echo_time() {
 
 clip_mode=false
 
-if [ "$#" = 2 ]; then
-    raster_paths=$1
-    clipper=$2
+if [ "$#" = 3 ]; then
+    input_file=$1
+    output_dir=$2
+    clipper=$3
 	clip_mode=true
-elif [ "$#" = 1 ]; then
-    raster_paths=$1
+elif [ "$#" = 2 ]; then
+    input_file=$1
+    output_dir=$2
 else
    	echo ""
     echo "Usage:"
-    echo "    gdal_create_optimized_dem.sh <input_file_list> [optional_clipper]"
+    echo "    gdal_create_optimized_dem.sh <input_file> <output_dir> [optional_clipper]"
     echo ""
     echo "Example:"
-    echo "    gdal_create_optimized_dem.sh my_raster_paths.txt my-area.geojson"
+    echo "    gdal_create_optimized_dem.sh my_input_file.vrt my-output-dir my-area.geojson"
 	echo ""
-	echo "Creates an optimized DEM file from files provided by my_raster_paths.txt clipped to cutline of file my-area.geojson."
-	echo "Clipping supports any OGR and multipolygon format (GeoJSON, Shapefile etc.) Area outside of HSL-area.geojson is written to NoData."
+	echo "Creates an optimized GeoTIFF DEM file from files provided by my_input_file.vrt clipped to cutline of file my-area.geojson in my-output-dir."
+	echo "Input and clipping supports any OGR and multipolygon format (GeoJSON, Shapefile etc.) Area outside of clipper is written to NoData."
+	echo "Reprojects to WGS84 and expects the input data to be in EPSG:3067."
 	exit 1
 fi
 
+input_file_basepath=${input_file##*/}
+input_file_basename=${input_file_basepath%.*}
+clipper_basepath=${clipper##*/}
+clipper_basename=${clipper_basepath%.*}
 
-source_vrt="dem-area-$(date +"%Y%m%d_%H%M%S").vrt"
-reprojected="reprojected-$(date +"%Y%m%d_%H%M%S").tif"
-finaldem="DEM-final-uint16-dm-$(date +"%Y%m%d_%H%M%S").tif"
-
-echo_time "1. Creating VRT of raster files."
-gdalbuildvrt -input_file_list $raster_paths $source_vrt
+initial_cut="$output_dir/$input_file_basename-initial_cut-$clipper_basename.tif"
+reproj="$output_dir/$input_file_basename-initial_cut-$clipper_basename-EPSG4326.tif"
+finaldem="$output_dir/$clipper_basename-DEM-final-uint16-dm.tif"
 
 if $clip_mode ; then
-    echo_time "2. Creating GeoTiff with cutline, nodata 99999"
-    gdalwarp -of GTiff -dstnodata 99999 -cutline $clipper -crop_to_cutline -multi -wo NUM_THREADS=ALL_CPUS -wo OPTIMIZE_SIZE -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW $source_vrt $reprojected
+    echo_time "1. Creating GeoTiff with cutline, nodata 99999"
+    gdalwarp -of GTiff -dstnodata 99999 -cutline $clipper -crop_to_cutline -multi -wo NUM_THREADS=ALL_CPUS -wo OPTIMIZE_SIZE -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW --config GDAL_CACHEMAX 500 -wm 500 $input_file $initial_cut
 else
-    echo_time "2. Creating GeoTiff, nodata to 99999"
-    gdalwarp -of GTiff -dstnodata 99999 -multi -wo NUM_THREADS=ALL_CPUS -wo OPTIMIZE_SIZE -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW $source_vrt $reprojected
+    echo_time "1. Creating GeoTiff, nodata to 99999"
+    gdalwarp -of GTiff -dstnodata 99999 -multi -wo NUM_THREADS=ALL_CPUS -wo OPTIMIZE_SIZE -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW --config GDAL_CACHEMAX 500 -wm 500 $input_file $initial_cut
 fi
 
-echo_time "3. Reproject to WGS84"
+echo_time "2. Reproject to WGS84"
 
-gdalwarp -of GTiff -srcnodata 99999 -dstnodata 99999 -s_srs EPSG:3067 -t_srs EPSG:4326 -r bilinear -multi -wo NUM_THREADS=ALL_CPUS -wo OPTIMIZE_SIZE -co COMPRESS=LZW -co BIGTIFF=YES -co TILED=YES $reprojected 4326-$reprojected
+gdalwarp -of GTiff -srcnodata 99999 -dstnodata 99999 -s_srs EPSG:3067 -t_srs EPSG:4326 -r bilinear -multi -wo NUM_THREADS=ALL_CPUS -wo OPTIMIZE_SIZE -co COMPRESS=LZW -co BIGTIFF=YES -co TILED=YES --config GDAL_CACHEMAX 500 -wm 500 $initial_cut $reproj
 
-echo_time "4. Converting elevation unit from meters to decimeters. Round 32 bit floats to 16 bit unsigned integers. NoData is 0."
+echo_time "3. Converting elevation unit from meters to decimeters. Round 32 bit floats to 16 bit unsigned integers. NoData is 0."
 
-gdal_calc.py -A 4326-$reprojected --outfile=$finaldem --type UInt16 --format=GTiff --co COMPRESS=LZW --co TILED=YES --co BIGTIFF=YES --co NUM_THREADS=ALL_CPUS --calc="numpy.around(10*(A*(A>0)))" --NoDataValue=0
+gdal_calc.py -A $reproj --outfile=$finaldem --type UInt16 --format=GTiff --co COMPRESS=LZW --co TILED=YES --co BIGTIFF=YES --co NUM_THREADS=ALL_CPUS --calc="numpy.around(10*(A*(A>0)))" --NoDataValue=0
 
 duration=$SECONDS
 echo_time "Finished gdal_create_optimized_dem.sh. $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
